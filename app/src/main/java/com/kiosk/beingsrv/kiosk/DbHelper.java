06689efcbef306054b16fb2653f86d624a178bac
Sourav Lahoti <sourav26721062@gmail.com>
2015-10-03 14:50:13 +0530

    Final commit an d bug fix

--- app/src/main/java/com/kiosk/beingsrv/kiosk/DbHelper.java
+++ app/src/main/java/com/kiosk/beingsrv/kiosk/DbHelper.java
@@ -15,14 +15,15 @@ import android.widget.Toast;
 public class DbHelper extends SQLiteOpenHelper {
 
     // Database attributes
-    public static final String DB_NAME = "Womenintech";
-    public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "database";
+    public static final int DB_VERSION = 2;
 
     // Table attributes
 
     public static final String TABLE_NAME = "Data_Table";
 
     //Data Table
+    public static final String KEY_ID = "id";
     public static final String KEY_FNAME ="First_Name";
     public static final String KEY_LNAME = "Last_Name";
     public static final String KEY_EMAIL = "Email_Address";
@@ -31,15 +32,14 @@ public class DbHelper extends SQLiteOpenHelper {
 
 
     public DbHelper(Context context) {
-        super(context, DB_NAME, null, 1);
+        super(context, DB_NAME, null, DB_VERSION);
         this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        try {
 
-        String DataTable = "create table if not exists " + TABLE_NAME + " ( " + BaseColumns._ID + " integer primary key autoincrement, "
+        String DataTable = "create table if not exists " + TABLE_NAME + " ( " + KEY_ID + " integer primary key, "
                 + KEY_FNAME + " text not null, "
                 + KEY_LNAME + " text, "
                 + KEY_EMAIL + " text, "
@@ -47,27 +47,6 @@ public class DbHelper extends SQLiteOpenHelper {
 
         db.execSQL(DataTable);
             Log.i("Executed inside", "table created");
-        }
-        catch (Exception e){
-            Log.i("table creation ",e.getMessage().toString());
-            Toast.makeText(context,"Table creation failed !!",Toast.LENGTH_SHORT).show();
-        }
-
-//        try {
-//            String ROW1 = "INSERT INTO " + TABLE_NAME + " ("
-//                    + KEY_FNAME + ", " + KEY_LNAME + ", "
-//                    + KEY_EMAIL + ", "
-//                    + KEY_CHECK + ") Values ('" + f_name + "','" + l_name + "','" + e_mail + "','" + checked + "')";
-//            db.execSQL(ROW1);
-//            Log.i("Executed inside", "response accepted");
-//            Toast.makeText(context, " Response is Accepted !!", Toast.LENGTH_SHORT).show();
-//        }
-//        catch (Exception e)
-//        {
-//            Log.i("Error Insert",e.getMessage().toString());
-//            Toast.makeText(context,"OOps something went wrong !! Please retry !!",Toast.LENGTH_SHORT).show();
-//        }
-
     }
 
     @Override
@@ -77,15 +56,21 @@ public class DbHelper extends SQLiteOpenHelper {
     }
 
     public void addEntry(UserData data ) {
-        SQLiteDatabase db = this.getWritableDatabase();
 
-        ContentValues values = new ContentValues();
-        values.put(KEY_FNAME, data.get_fname());
-        values.put(KEY_FNAME, data.get_lname());
-        values.put(KEY_EMAIL, data.get_email());
-        values.put(KEY_CHECK, data.get_updatecheck());
-        db.insert(TABLE_NAME, null, values);
-        db.close(); // Closing database connection
-        Toast.makeText(context, " Response is Accepted !!", Toast.LENGTH_SHORT).show();
+        try {
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put(KEY_FNAME, data.get_fname());
+            values.put(KEY_LNAME, data.get_lname());
+            values.put(KEY_EMAIL, data.get_email());
+            values.put(KEY_CHECK, data.get_updatecheck());
+            db.insert(TABLE_NAME, null, values);
+            db.close(); // Closing database connection
+            Toast.makeText(context, " Thanks !! Response Accepted ...", Toast.LENGTH_SHORT).show();
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(context, " OOps something went wrong ...", Toast.LENGTH_SHORT).show();
+        }
     }
 }
